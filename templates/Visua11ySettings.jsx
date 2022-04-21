55ea108fa0d1d9e6f7a7faacceb2d168c36982b0
Joseph Replin <joe.replin@kineo.com>
2022-04-21 13:18:57 -0500

    Update Visua11ySettings.jsx

--- templates/Visua11ySettings.jsx
+++ templates/Visua11ySettings.jsx
@@ -36,7 +36,7 @@ export default function Visua11ySettings(config) {
         <div className='visua11ysettings__group visua11ysettings__group-visualdisplay' role='group' aria-labelledby='visualdisplay'>
           {/* Should 'visua11ysettings__group-title' read as a title or is the lablled list enough? */}
           {config._groups.visualDisplay &&
-          <div className='visua11ysettings__group-title' id='visualdisplay' role="heading" aria-level="3">
+          <div className='visua11ysettings__group-title' id='visualdisplay' role="heading" aria-level="1">
             {config._groups.visualDisplay}
           </div>
           }
@@ -107,7 +107,7 @@ export default function Visua11ySettings(config) {
 
         <div className='visua11ysettings__group visua11ysettings__group-distractions' role='group' aria-labelledby='distractions'>
           {config._groups.distractions &&
-          <div className='visua11ysettings__group-title' id='distractions' role="heading" aria-level="3">
+          <div className='visua11ysettings__group-title' id='distractions' role="heading" aria-level="1">
             {config._groups.distractions}
           </div>
           }
@@ -142,7 +142,7 @@ export default function Visua11ySettings(config) {
 
         <div className='visua11ysettings__group visua11ysettings__group-readability' role='group' aria-labelledby='readability'>
           {config._groups.readability &&
-          <div className='visua11ysettings__group-title' id='readability' role="heading" aria-level="3">
+          <div className='visua11ysettings__group-title' id='readability' role="heading" aria-level="1">
             {config._groups.readability}
           </div>
           }
